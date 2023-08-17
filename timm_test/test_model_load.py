bbf3a3363c379220b8287cd3d6b5fd3fe557deb9
xbkaishui <xxkaishui@gmail.com>
2023-08-17 08:38:57 +0800

    fix data load

--- timm_test/test_model_load.py
+++ timm_test/test_model_load.py
@@ -8,19 +8,29 @@ def load_model_with_ckpt():
     ptcfg = timm.models.get_pretrained_cfg('wide_resnet50_2')
     ptcfg.url = None
     ckpt_file = '/root/.cache/ckpts/wide_resnet50_racm-8234f177.pth'
-    # ptcfg.file = '/root/.cache/huggingface/hub/models--timm--wide_resnet50_2.racm_in1k'
     ptcfg.file = ckpt_file
+    # ptcfg.file = '/root/.cache/huggingface/hub/models--timm--wide_resnet50_2.racm_in1k'
     logger.info(ptcfg)
     m = timm.create_model(
                 backbone,
-                # pretrained_cfg=ptcfg,
+                pretrained_cfg=ptcfg,
                 pretrained=pre_trained,
                 features_only=True,
                 exportable=True,
                 out_indices=[1, 2, 3],
             )
-    logger.info(m.classifier.weight)
+    logger.info(m._modules['conv1'].weight)
     # logger.info(model)
+    
+    m = timm.create_model(
+                backbone,
+                pretrained_cfg=ptcfg,
+                pretrained=pre_trained,
+                features_only=True,
+                exportable=True,
+                out_indices=[1, 2, 3],
+            )
+    logger.info(m._modules['conv1'].weight)
     ...
     
 if __name__ == '__main__':
